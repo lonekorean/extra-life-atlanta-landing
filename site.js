e0b677c450d05307da1c643c05cbf8dfb232677b
Will Boyd <wboyd@salesforce.com>
2016-10-02 16:48:58 -0400

    For input styles

---
+++ site.js
@@ -0,0 +1,14 @@
+$(function() {
+	$('.input-row input').on({
+		focus: focusInput,
+		blur: blurInput
+	});
+
+	function focusInput() {
+		$(this).closest('.input-row').addClass('focused');
+	}
+
+	function blurInput() {
+		$(this).closest('.input-row').removeClass('focused');
+	}
+})
